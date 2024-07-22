6eaac0c343adfa519dafd4f72c49f6ff59d6002a
pathiphon <pathiphon.j@muze.co.th>
2024-07-22 14:14:19 +0700

    Update app icon & splash screen.

--- composeApp/src/androidMain/kotlin/com/adedom/mycompose/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/adedom/mycompose/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.adedom.mycompose.ui.theme.MyComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
